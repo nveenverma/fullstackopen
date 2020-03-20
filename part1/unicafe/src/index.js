6360a463435d1207d978649d0b458ae91f07cc71
Naveen Verma <nveenverma@gmail.com>
2020-03-20 13:35:17 +0400

    Ex 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -11,34 +11,45 @@ const Button = ({changeFunction, text}) => {
 
 const P = ({text, value}) => <p><b>{text} {value}</b></p>
 
+const Statistics = ({good, bad, neutral, 
+    all, avg, positivePercentage}) => {
+    return (
+        <>
+        <h1>statistics</h1>
+        <P text="good" value={good}/>
+        <P text="neutral" value={neutral}/>
+        <P text="bad" value={bad}/>
+        <P text="all" value={all}/>
+        <P text="average" value={avg} />
+        <P text="positive" value={positivePercentage} />
+        </>
+    )
+}
+
 const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const all = good + bad + neutral;
-  const avg = (good-bad)/all;
-  const positivePercentage = (good/all)*100 + " %";
 
-  const incrementGood = () => setGood(good+1)
-  const incrementNeutral = () => setNeutral(neutral+1)
-  const incrementBad = () => setBad(bad+1)
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    const all = good + bad + neutral;
+    const avg = (good-bad)/all;
+    const positivePercentage = (good/all)*100 + " %";
 
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <Button changeFunction={incrementGood} text="good" />
-      <Button changeFunction={incrementNeutral} text="neutral" />
-      <Button changeFunction={incrementBad} text="bad" />
+    const incrementGood = () => setGood(good+1)
+    const incrementNeutral = () => setNeutral(neutral+1)
+    const incrementBad = () => setBad(bad+1)
 
-      <h1>statistics</h1>
-      <P text="good" value={good}/>
-      <P text="neutral" value={neutral}/>
-      <P text="bad" value={bad}/>
-      <P text="all" value={all}/>
-      <P text="average" value={avg} />
-      <P text="positive" value={positivePercentage} />
-    </div>
+    return (
+        <div>
+            <h1>give feedback</h1>
+            <Button changeFunction={incrementGood} text="good" />
+            <Button changeFunction={incrementNeutral} text="neutral" />
+            <Button changeFunction={incrementBad} text="bad" />
+
+            <Statistics good={good} bad={bad} neutral={neutral} 
+            all={all} avg={avg} positivePercentage={positivePercentage} />
+
+        </div>
   )
 }
 
