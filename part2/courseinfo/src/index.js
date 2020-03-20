f0ff58e036ce33072cd58f1a6e0b8fa7bc832c41
Naveen Verma <nveenverma@gmail.com>
2020-03-21 01:44:36 +0400

    Ex 2.1

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,57 @@
+// jshint esversion:6
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({headerContent}) => <h1>{headerContent}</h1>;
+
+const Part = ({p, ex}) => <p>{p} {ex}</p>
+
+const Content = ({parts}) => {
+  return (
+      <>
+      {parts.map((i, key) => <Part key={key} p={i.name} ex={i.exercises} />)}
+      </>
+  )
+}
+
+const Course = ({content}) => {
+  return (
+    <div>
+      <Header headerContent={content.name} />
+      <Content parts={content.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  
+  return (
+    <div>
+      <Course content={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
