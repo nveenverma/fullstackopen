da6e708f172f181655106930d5c1b540453a9745
Naveen Verma <nveenverma@gmail.com>
2020-03-21 23:45:16 +0400

    Ex 2.6

---
+++ part2/thephonebook/src/App.js
@@ -0,0 +1,44 @@
+// jshint esversion:6
+
+import React, { useState } from 'react';
+import Note from './components/Note';
+
+const App = () => {
+  const [ persons, setPersons] = useState([{ name: 'Arto Hellas' }]); 
+  const [ newName, setNewName ] = useState('');
+
+  const addNote = (e) => {
+    e.preventDefault();
+    const noteObject = {
+      name: newName
+    };
+
+    setPersons(persons.concat(noteObject));
+    setNewName('');
+  };
+  
+  const handleNoteChange = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNoteChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addNote}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        <ul>
+          {persons.map((i, key) => <Note key= {i.name} note={i} />)}
+        </ul>
+    </div>
+  )
+}
+
+export default App
---
+++ part2/thephonebook/src/components/Note.js
@@ -0,0 +1,7 @@
+// jshint esversion:6
+
+import React from 'react';
+
+const Note = ({ note }) => <li>{note.name}</li>
+
+export default Note
