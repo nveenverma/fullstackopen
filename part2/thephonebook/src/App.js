0b2004871f17d41d5a9a5f32152e6d9c5611a653
Naveen Verma <nveenverma@gmail.com>
2020-03-23 22:53:27 +0400

    Ex 2.11

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,20 +1,27 @@
 // jshint esversion:6
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './components/Filter';
 import AddContactForm from './components/AddContactForm';
 import ContactsList from './components/ContactsList';
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]); 
+  const [ persons, setPersons] = useState([]);
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ showNotes, setShowNotes ] = useState('');
+
+  useEffect(() => {
+    console.log("effect");
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('Promise fullfilled');
+        setPersons(response.data);        
+      })
+  }, []);
+  
 
   const addNote = (e) => {
     e.preventDefault();
